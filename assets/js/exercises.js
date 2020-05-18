60c63edf15cfeb281ae6d4ee6144a8794e410773
jbjulia <joseph.b.julian@gmail.com>
2020-05-18 20:07:20 +0100

    Major improvements and code cleanup

--- assets/js/exercises.js
+++ assets/js/exercises.js
@@ -1,19 +1,17 @@
-function exercises() {
-    a = parent.document.URL.substring(parent.document.URL.indexOf('index='), parent.document.URL.length);
+a = parent.document.URL.substring(parent.document.URL.indexOf('index='), parent.document.URL.length);
 
-    if (a === 'index=upper') {
-        document.getElementById("upper-body").className = "nav-link active";
-    } else if (a === 'index=lower') {
-        document.getElementById("lower-body").className = "nav-link active";
-    } else if (a === 'index=core') {
-        document.getElementById("core").className = "nav-link active";
-    } else if (a === 'index=cardio') {
-        document.getElementById("cardio").className = "nav-link active";
-    } else if (a === 'index=stretches') {
-        document.getElementById("stretches").className = "nav-link active";
-    } else if (a === 'index=nutrition') {
-        document.getElementById("nutrition").className = "nav-link active";
-    } else {
-        document.getElementById("upper-body").className = "nav-link active";
-    }
+if (a === 'index=upper') {
+    document.getElementById("upper-body").className = "nav-link active";
+} else if (a === 'index=lower') {
+    document.getElementById("lower-body").className = "nav-link active";
+} else if (a === 'index=core') {
+    document.getElementById("core").className = "nav-link active";
+} else if (a === 'index=cardio') {
+    document.getElementById("cardio").className = "nav-link active";
+} else if (a === 'index=stretches') {
+    document.getElementById("stretches").className = "nav-link active";
+} else if (a === 'index=nutrition') {
+    document.getElementById("nutrition").className = "nav-link active";
+} else {
+    document.getElementById("upper-body").className = "nav-link active";
 }
